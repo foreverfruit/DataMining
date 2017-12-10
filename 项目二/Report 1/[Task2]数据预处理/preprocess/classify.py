793b48e70940bb25a895047690d190a87bcb8a1b
heyufeng <foreverfruit@126.com>
2017-12-10 16:46:59 +0800

    根据距离的分类的几种方法比较

--- 项目二/Report 1/[Task2]数据预处理/preprocess/classify.py
+++ 项目二/Report 1/[Task2]数据预处理/preprocess/classify.py
@@ -8,6 +8,7 @@
 import numpy as np
 from sklearn import neighbors
 import  preprocess as pp
+import math
 
 # 标称值映射关系
 map_workclass = {1.0: 0.21879206676837476, 2.0: 0.2857142857142857, 3.0: 0.55865921787709494, 4.0: 0.32358803986710966, 5.0: 0.26896012509773259, 6.0: 0.0}
@@ -115,7 +116,9 @@ def my_classify1(train_filename:str,test_filename:str):
     train = np.loadtxt(fname=train_filename + '.done', delimiter=',', dtype=float, unpack=False)
     train_a = train[np.where(train[:, 14] == 0)]
     train_b = train[np.where(train[:, 14] == 1)]
-    print('a类占比:',train_a.shape[0]/train.shape[0])
+    size_a,size_b = train_a.shape[0],train_b.shape[0]
+    print('a类占比:',size_a/train.shape[0])
+
 
     # 测试集
     test = np.loadtxt(fname=test_filename + '.done', delimiter=',', dtype=float, unpack=False)
@@ -158,7 +161,7 @@ def my_classify1(train_filename:str,test_filename:str):
                 F_b+=f
 
             # 合力比较，乘以权重
-            var_predict+=(F_a-F_b)*cors[j]
+            var_predict+=(F_a/size_a-F_b/size_b)*abs(cors[j])
 
         # 预测的类标号
         predict_class = 0
@@ -173,7 +176,7 @@ def my_classify1(train_filename:str,test_filename:str):
         elif var_predict<0:
             predict_class = 1
 
-        print('预测类：',predict_class,'预测值：',var_predict,'真实类：',class_test)
+        print('结果：','正确' if predict_class==class_test else '错误','预测类：',predict_class,'预测值：',var_predict,'真实类：',class_test)
         if(predict_class == class_test):
             count += 1
 
@@ -181,11 +184,258 @@ def my_classify1(train_filename:str,test_filename:str):
     print('测试总数：',size,'失败率：',fail_count/size,'准确率:',count/size)
 
 
+def my_classify2(train_filename:str,test_filename:str):
+    '''
+    我的分类算法：距离法，高维空间中点的平均距离做比较，距离越小，表示越吻合
+    准确率：100%
+    '''
+    # 训练集
+    train = np.loadtxt(fname=train_filename + '.done', delimiter=',', dtype=float, unpack=False)
+    train_a = train[np.where(train[:, 14] == 0)]
+    train_b = train[np.where(train[:, 14] == 1)]
+    size_a,size_b = train_a.shape[0],train_b.shape[0]
+    print('a类占比:',size_a/train.shape[0])
+
+    # 测试集
+    test = np.loadtxt(fname=test_filename + '.done', delimiter=',', dtype=float, unpack=False)
+
+    count = 0
+    fail_count = 0
+    # 遍历测试每一个测试数据
+    size_test = test.shape[0]
+    for index in range(size_test):
+        element_test = test[index]
+        # 当前测试对象额实际类标号
+        class_test = element_test[14]
+        # 到两个类的平均距离
+        d_a,d_b = 0,0
+        # 遍历到两个类的训练样本的平均距离
+        for element_train in train_a:
+            minus = element_test-element_train
+            minus = np.square(minus)
+            d_a += np.sqrt(minus.sum())
+
+        for element_train in train_b:
+            minus = element_test-element_train
+            minus = np.square(minus)
+            d_b += np.sqrt(minus.sum())
+
+        # 预测的类标号
+        predict_class = 0
+        # 计量结果
+        if d_a/size_a == d_b/size_b:
+            # 预测失败
+            print('failed!')
+            fail_count += 1
+        elif d_a/size_a < d_b/size_b:
+            # 预测为a类，标号为0
+            predict_class = 0
+        elif d_a/size_a > d_b/size_b:
+            predict_class = 1
+
+        print('结果：','正确' if predict_class==class_test else '错误','预测类：',predict_class,'真实类：',class_test)
+        if(predict_class == class_test):
+            count += 1
+
+    #size_test = test.shape[0]
+    print('测试总数：',size_test,'失败率：',fail_count/size_test,'准确率:',count/size_test)
+
+
+def my_classify3(train_filename:str,test_filename:str):
+    '''
+    我的分类算法：距离法，高维空间中点的平均距离做比较，距离越小，表示越吻合
+    方法二的基础上，提速，方法：合成向量（各维累加）
+    1.各维相加，准确率：0.533，显然太简单，物理解释也能说明这种方法的不合理，距离会随着各维值的累加而急剧增加，最终严重受样本容量的影响
+    2.各维取平均，classify4
+    '''
+    # 训练集
+    train = np.loadtxt(fname=train_filename + '.done', delimiter=',', dtype=float, unpack=False)
+    train_a = train[np.where(train[:, 14] == 0)]
+    train_b = train[np.where(train[:, 14] == 1)]
+
+    # 类统计
+    size_a,size_b = train_a.shape[0],train_b.shape[0]
+    print('a类占比:',size_a/train.shape[0])
+
+    # 各类的向量和
+    train_a_sum = np.sum(train_a,axis=0)
+    train_b_sum = np.sum(train_b,axis=0)
+
+    # 测试集
+    test = np.loadtxt(fname=test_filename + '.done', delimiter=',', dtype=float, unpack=False)
+
+    count = 0
+    fail_count = 0
+    # 遍历测试每一个测试数据
+    size_test = test.shape[0]
+    for index in range(size_test):
+        element_test = test[index]
+        # 当前测试对象额实际类标号
+        class_test = element_test[14]
+        # 到两个类的平均距离
+        d_a,d_b = 0,0
+        # 遍历到两个类的训练样本的平均距离
+        element_a = element_test*size_a
+        element_b = element_test*size_b
+        d_a = np.sqrt(np.square(element_a-train_a_sum).sum())
+        d_b = np.sqrt(np.square(element_b-train_b_sum).sum())
+
+        # 预测的类标号
+        predict_class = 0
+        # 计量结果
+        if d_a == d_b:
+            # 预测失败
+            print('failed!')
+            fail_count += 1
+        elif d_a < d_b:
+            # 预测为a类，标号为0
+            predict_class = 0
+        elif d_a > d_b:
+            predict_class = 1
+
+        print('结果：','正确' if predict_class==class_test else '错误','预测类：',predict_class,'真实类：',class_test)
+        if(predict_class == class_test):
+            count += 1
+
+    #size_test = test.shape[0]
+    print('测试总数：',size_test,'失败率：',fail_count/size_test,'准确率:',count/size_test)
+
+
+def my_classify4(train_filename:str,test_filename:str):
+    '''
+    我的分类算法：距离法，高维空间中点的平均距离做比较，距离越小，表示越吻合
+    方法3的改进
+    '''
+    # 训练集
+    train = np.loadtxt(fname=train_filename + '.done', delimiter=',', dtype=float, unpack=False)
+    train_a = train[np.where(train[:, 14] == 0)]
+    train_b = train[np.where(train[:, 14] == 1)]
+
+    # 类统计
+    size_a,size_b = train_a.shape[0],train_b.shape[0]
+    print('a类占比:',size_a/train.shape[0])
+
+    # 各类的平均向量
+    train_a_avg = np.sum(train_a,axis=0)/size_a
+    train_b_avg = np.sum(train_b,axis=0)/size_b
+
+    # 测试集
+    test = np.loadtxt(fname=test_filename + '.done', delimiter=',', dtype=float, unpack=False)
+
+    count = 0
+    fail_count = 0
+    # 遍历测试每一个测试数据
+    size_test = test.shape[0]
+    for index in range(size_test):
+        element_test = test[index]
+        # 当前测试对象额实际类标号
+        class_test = element_test[14]
+        # 到两个类的平均距离
+        d_a,d_b = 0,0
+        d_a = np.sqrt(np.square(element_test-train_a_avg).sum())
+        d_b = np.sqrt(np.square(element_test-train_b_avg).sum())
+
+        # 预测的类标号
+        predict_class = 0
+        # 计量结果
+        if d_a == d_b:
+            # 预测失败
+            print('failed!')
+            fail_count += 1
+        elif d_a < d_b:
+            # 预测为a类，标号为0
+            predict_class = 0
+        elif d_a > d_b:
+            predict_class = 1
+
+        print('结果：','正确' if predict_class==class_test else '错误','预测类：',predict_class,'真实类：',class_test)
+        if(predict_class == class_test):
+            count += 1
+
+    #size_test = test.shape[0]
+    print('测试总数：',size_test,'失败率：',fail_count/size_test,'准确率:',count/size_test)
+
+
+
+def my_classify5(train_filename:str,test_filename:str):
+    '''
+    我的分类算法：距离法
+    各维度平均距离，根据相关度做加权
+    '''
+    # 训练集
+    train = np.loadtxt(fname=train_filename + '.done', delimiter=',', dtype=float, unpack=False)
+    train_a = train[np.where(train[:, 14] == 0)]
+    train_b = train[np.where(train[:, 14] == 1)]
+
+    # 类统计
+    size_a,size_b = train_a.shape[0],train_b.shape[0]
+    print('a类占比:',size_a/train.shape[0])
+
+    # 各类的平均向量
+    train_a_avg = np.sum(train_a,axis=0)/size_a
+    train_b_avg = np.sum(train_b,axis=0)/size_b
+
+    # 测试集
+    test = np.loadtxt(fname=test_filename + '.done', delimiter=',', dtype=float, unpack=False)
+
+    cors = []
+    # 计算训练集每个维度相关度
+    for i in range(train.shape[1] - 1):
+        if i == 11:
+            # 跳过列11，但是为了后面的计算，这个值还是要占个位，否则后续的计算会数组越界
+            cors.append(0)
+            continue
+        cors.append(np.corrcoef(train[:, i], train[:, 14])[0, 1])
+
+    count = 0
+    fail_count = 0
+    # 遍历测试每一个测试数据
+    size_test = test.shape[0]
+    for index in range(size_test):
+        element_test = test[index]
+        # 当前测试对象实际类标号
+        class_test = element_test[14]
+        # 当前测试对象的预测标号，大于0表示a类，小于0表示b类
+        var_predict = 0
+        # 遍历每一个属性（维度）
+        for j in range(test.shape[1] - 1):
+            # 忽略列为11的维度
+            if j == 11:
+                continue
+
+            # 距离比较，乘以权重
+            var_predict += (abs(element_test[j]-train_a_avg[j]) - abs(element_test[j]-train_b_avg[j])) * abs(cors[j])
+
+        # 预测的类标号
+        predict_class = 0
+        # 计量结果
+        if var_predict == 0:
+            # 预测失败
+            print('failed!')
+            fail_count += 1
+        elif var_predict > 0:
+            # 预测为a类，标号为0,计算是a的距离减去b的距离做加权，大于0，表示离a比较远，更倾向与b，b的类标号为1
+            predict_class = 1
+        elif var_predict < 0:
+            predict_class = 0
+
+        print('结果：', '正确' if predict_class == class_test else '错误', '预测类：', predict_class, '预测值：', var_predict, '真实类：',
+              class_test)
+        if (predict_class == class_test):
+            count += 1
+
+    #size_test = test.shape[0]
+    print('测试总数：',size_test,'失败率：',fail_count/size_test,'准确率:',count/size_test)
+
 
 if __name__ == '__main__':
     filename = 'adult.test'
     pp.discard(filename)
     data = handle_normal(filename)
     data = handle_continuous(data, filename)
-    #do_classify('adult.data',filename)
-    my_classify1('adult.data',filename)
+    #do_classify('adult.data',filename)      # 81%，scikit-learn的KNN调用
+    #my_classify1('adult.data',filename)     # 76%，错误的模型……
+    #my_classify2('adult.data',filename)     # 100%，速度巨慢，每一个测试实体计算对所有训练实体的距离再求平均
+    #my_classify3('adult.data',filename)     # 53%，训练向量求和，将测试向量相应扩大size倍，再求一次距离，距离剧烈扩大，结果显然不正确
+    #my_classify4('adult.data',filename)     # 100% 且速度最快，秒出结果，先求训练总体的平均向量，再求一次距离即可
+    my_classify5('adult.data',filename)      # 72%，各维距离=测试样本各维度值-各维度训练样本均值，再根据各维度相关度加权，最后属于距离较小的类
